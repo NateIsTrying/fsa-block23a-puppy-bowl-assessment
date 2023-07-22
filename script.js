bef2f7d6e725472eb7f8d412fb989e1f90e032ff
Nathan Kim <novicenate@pop-os.localdomain>
2023-07-22 11:42:47 -0400

    cleaned js code, improved css but still very basic, will continue to clean

--- script.js
+++ script.js
@@ -2,13 +2,8 @@
 const cohortName = "2306-FSA-ET-WEB-FT-SF";
 const API_URL = `https://fsa-puppy-bowl.herokuapp.com/api/${cohortName}`;
 
-/**
- * Fetches all players from the API.
- * @returns {Object[]} the array of player objects
- */
 const fetchAllPlayers = async () => {
   try {
-    // TODO
     const response = await fetch(`${API_URL}/players`);
     const puppiesData = await response.json();
     const allPlayerData = puppiesData.data.players;
@@ -18,19 +13,11 @@ const fetchAllPlayers = async () => {
   }
 };
 
-/**
- * Fetches a single player from the API.
- * @param {number} playerId
- * @returns {Object} the player object
- */
-
 const fetchSinglePlayer = async (playerId) => {
   try {
-    // TODO
     const response = await fetch(`${API_URL}/players/${playerId}`);
     const puppyData = await response.json();
     const singlePlayerData = puppyData.data.player;
-    // console.log(singlePlayerData);
 
     if(!singlePlayerData){
       console.log(`Player #${playerId} not found.`);
@@ -43,15 +30,8 @@ const fetchSinglePlayer = async (playerId) => {
   }
 };
 
-
-/**
- * Adds a new player to the roster via the API.
- * @param {Object} playerObj the player to add
- * @returns {Object} the player returned by the API
- */
 const addNewPlayer = async (playerObj) => {
   try {
-    // TODO
     const response = await fetch(`${API_URL}/players`, {
       method: 'POST',
       body: JSON.stringify(playerObj),
@@ -66,10 +46,6 @@ const addNewPlayer = async (playerObj) => {
   }
 };
 
-/**
- * Removes a player from the roster via the API.
- * @param {number} playerId the ID of the player to remove
- */
 const removePlayer = async (playerId) => {
     console.log(`${playerId} has been removed.`);
     const response = await fetch(`${API_URL}/players/${playerId}`, {
@@ -77,30 +53,9 @@ const removePlayer = async (playerId) => {
     });
     const result = await response.json();
     console.log(`It's ${result.success}, the result was a success.`);
-    // renderAllPlayers(players);
 };
 
-/**
- * Updates `<main>` to display a list of all players.
- *
- * If there are no players, a corresponding message is displayed instead.
- *
- * Each player is displayed in a card with the following information:
- * - name
- * - id
- * - image (with alt text of the player's name)
- *
- * Additionally, each card has two buttons:
- * "See details" button that, when clicked, calls `renderSinglePlayer` to
- * display more information about the player
-  "Remove from roster" button that, when clicked, will call `removePlayer` to
-   remove that specific player and then re-render all players
-
- * Note: this function should replace the current contents of `<main>`, not append to it.
- * @param {Object[]} playerList - an array of player objects
- */
 const renderAllPlayers = (playerList) => {
-  // TODO
   try {
     const main = document.querySelector('main');
     main.innerHTML = ``;
@@ -108,12 +63,15 @@ const renderAllPlayers = (playerList) => {
       const section = document.createElement('section');
       main.appendChild(section);
       section.innerHTML = `
-      <h3>${player.name}</h3>
-      <h5>Breed: ${player.breed}</h5>
-      <img src='${player.imageUrl}'/>
-    
-      <button class='details-button' data-id=${player.id}>See Details</button>
-      <button class='delete-button' data-id=${player.id}>Remove from Roster</button>
+
+      <div class='player-info'>
+        <h3>${player.name}</h3>
+        <h5>Breed: ${player.breed}</h5>
+
+        <img src='${player.imageUrl}'/>
+        <button class='details-button' data-id=${player.id}>See Details</button>
+        <button class='delete-button' data-id=${player.id}>Remove from Roster</button>
+      </div>
       `
     });
 
@@ -145,24 +103,7 @@ const renderAllPlayers = (playerList) => {
   }
 };
 
-
-
-
-/**
- * Updates `<main>` to display a single player.
- * The player is displayed in a card with the following information:
- * - name
- * - id
- * - breed
- * - image (with alt text of the player's name)
- * - team name, if the player has one, or "Unassigned"
- *
- * The card also contains a "Back to all players" button that, when clicked,
- * will call `renderAllPlayers` to re-render the full list of players.
- * @param {Object} player an object representing a single player
- */
 const renderSinglePlayer = async (playerId) => {
-  // TODO
   try {
     const main = document.querySelector('main');
     main.innerHTML = '';
@@ -178,13 +119,16 @@ const renderSinglePlayer = async (playerId) => {
     const section = document.createElement('section');
     main.appendChild(section);
     section.innerHTML = `
-      <h3>${player.name}</h3>
-      <h5>Breed: ${player.breed}</h5>
-      <img src='${player.imageUrl}'/>
 
-      <button class='return-button'>Return</button>
-      <button class='delete-button' data-id=${player.id}>Remove from Roster</button>
-    `;
+      <div class='player-info'>
+        <h3>${player.name}</h3>
+        <h5>Breed: ${player.breed}</h5>
+
+        <img src='${player.imageUrl}'/>
+        <button class='return-button'>Return</button>
+        <button class='delete-button' data-id=${player.id}>Remove from Roster</button>
+      </div>
+      `;
   
     const returnButton = document.querySelector('.return-button');
     returnButton.addEventListener('click', async() => {
@@ -201,18 +145,10 @@ const renderSinglePlayer = async (playerId) => {
   }
 };
 
-/**
- * Fills in `<form id="new-player-form">` with the appropriate inputs and a submit button.
- * When the form is submitted, it should call `addNewPlayer`, fetch all players,
- * and then render all players to the DOM.
- */
-
 const renderNewPlayerForm = async () => {
   try {
-    // TODOAPI_URL
     const response = await fetch (`${API_URL}/teams`);
     const result = await response.json();
-    // console.log(result);
 
     let formHTML = `
       <h3>Add a new puppy!</h3>
@@ -223,7 +159,8 @@ const renderNewPlayerForm = async () => {
       <label for='imageUrl'>Image URL:</label>
       <input type='text' id='imageUrl' name='image Url' placeholder='Google Images' required>
       <button type='submit' > Add a new Puppy!</button>
-    `
+    `;
+
     const form = document.querySelector('form');
     form.innerHTML = formHTML;
 
@@ -241,7 +178,6 @@ const submitHandler = async (event) => {
   const name = form.name.value;
   const breed = form.breed.value;
   const img = form.imageUrl.value;
-  console.log(img)
 
   const data = await addNewPlayer({
     name,
@@ -249,10 +185,6 @@ const submitHandler = async (event) => {
     imageUrl: img, 
     status: 'bench',
   }) ;
-
-  // if(data){
-  //   alert(`${data.name} was created with ID: ${data.id}`)
-  // }
 
   const players = await fetchAllPlayers();
   renderAllPlayers(players);
@@ -262,26 +194,10 @@ const submitHandler = async (event) => {
   form.imageUrl.value = ''
 }
 
-/**
- * Initializes the app by fetching all players and rendering them to the DOM.
- */
 const init = async () => {
-
-  // addNewPlayer({
-  //   name: 'Cujo',
-  //   breed: 'Saint Bernard',
-  //   imageUrl: 'https://upload.wikimedia.org/wikipedia/en/d/de/Beethoven%271992.jpg'
-  // })
-
-  // removePlayer(13390);
-
   const players = await fetchAllPlayers();
-  console.log(players);
-  // await fetchSinglePlayer();
+  
   renderAllPlayers(players);
-  // renderSinglePlayer(players[0])
-  // renderNewPlayerForm();
-
   renderNewPlayerForm()
 };
 
